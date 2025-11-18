20c9d7e2cdca564a0438340c9c7be35e5c3b2bec
YiXuan Zhang <107227382@qq.com>
2025-11-18 22:54:03 +0800

    Add files via upload

---
+++ hello bubble sort.cpp
@@ -0,0 +1,34 @@
+﻿// ConsoleApplication72.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#pragma warning(disable:4996)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int str[8];
+	printf("Please type 8 numbers");
+	int i,m,n;
+	for (i = 0; i < 8; i++)
+	{
+		scanf("%d", &str[i]);
+	}
+	int temp = 0;
+	int min[8];
+	for (m = 0; m < 8; m++)
+	{
+		for (n = m + 1; n < 8; n++)
+		{
+			if (str[n] < str[m])
+			{
+				temp = str[n];
+				str[n] = str[m];
+				str[m] = temp;
+			}
+		}
+	}
+	for (i = 0; i < 8; i++)
+	{
+		printf("%d ", str[i]);
+	}
+}
